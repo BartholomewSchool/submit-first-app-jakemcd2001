665d8658140bc93d65df5d3e8cb829af93360dd9
Jake <jakemcd1234@gmail.com>
2017-04-10 09:42:26 +0100

    Create footyquiz.java

---
+++ footyquiz.java
@@ -0,0 +1,54 @@
+public class Program{
+	    Package stringvars; {
+	 import Java.util.Scanner; 
+	 //line above is not working for an unknown reason//
+    class test {
+   public static void main(String[ ] args) {
+      
+
+Scanner
+ 
+ sc = new Scanner(System.in);
+      String st = sc.
+
+getLine
+ 
+();
+   }
+}
+	   {
+	int Score0 = 0;
+	int Score = 0;
+	System.out.println("The Premier League Football Test");
+    System.out.print(Score);
+	System.out.println("Score:"); 
+	System.out.println(Score);
+	System.out.println("");
+	System.out.println("");
+	System.out.println("(Answer is terms or 'A','B','C','D' or 'E')");
+	System.out.println("Who was the top scorer of the 2015-2016 Premier League season?");
+	System.out.println("A. Diego Costa");
+	System.out.println("B. Harry Kane");
+	System.out.println("C. Jamie Vardy");
+	System.out.println("D. Sergio Aguero");
+	System.out.println("E. Romelu Lukaku");
+String Answer;
+System.out.print("Enter your answer: ");
+Answer = User_input.next( );
+	if(Answer == "B") {
+	    int NewScore = Score0 + 10;
+	    System.out.println("Correct! In that season, Harry Kane (Tottenham Hotspurs) scored 22! 3 goals more than the runner up and Premier League title champion, Jamie Vardy (Leicester City) with 19 goals. +10 points");
+	} 
+	else if(Answer == "C") {
+	    int NewScore = Score0 + 2;
+	    System.out.println("Incorrect! In that season, Harry Kane (Tottenhame Hotspurs) scored 3 more goals than Jamie Vardy (Leicester City) to become that seasons top scorer. But because you got the runner up, have 2 points. +2 points");
+	} 
+	else {
+	    System.out.println("Incorrect! Despite Diego Costa (Chelsea) scoring 11 goals, Jamie Vardy (Leicester City) scoring 19 goals, Sergoi Aguero (Manchester City) scoring 17 goals and Romelu Lukaku (Everton) scoring 18 goals. Harry Kane (Tottenham Hotspurs) was the top scorer with 22 goals in the 2015-16 Premier League season! +0 points");
+	}
+	System.out.print("NewScore");
+	
+	   
+	   }
+	}
+}
